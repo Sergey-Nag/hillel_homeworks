324fe96ec6c53513a518edd5dcff339ca3884118
Sergey-Nag <nagornyj.seriozha2010@gmail.com>
2020-10-08 00:14:12 +0300

    hw 3 + bonus

--- HW_3/calculator/main.js
+++ HW_3/calculator/main.js
@@ -1,27 +1,33 @@
-'use strict';
+"use strict";
 
-const inputNumber1 = prompt('Запрос (1/3):\nВведите первое число для операции', '');
-const inputOperator = prompt('Запрос (2/3):\nВведите оператор — (+, -, *, /, **)', '');
-const inputNumber2 = prompt('Запрос (3/3):\nВведите второе число для операции', '');
-
+const inputNumber1 = prompt("Запрос (1/3):\nВведите первое число для операции", "");
+const inputOperator = prompt("Запрос (2/3):\nВведите оператор — (+, -, *, /, **)", "");
+const inputNumber2 = prompt("Запрос (3/3):\nВведите второе число для операции", "");
 
 alert(returnMathResult(inputNumber1, inputNumber2, inputOperator));
 
-function returnMathResult(num1, num2, operator) {
-    let result = 'Ответ:\n'+ num1 + operator + num2 + '= ';
+function returnMathResult(str1, str2, operator) {
+  let result = "Ответ:\n" + str1 + operator + str2 + "= ";
 
-    if (num1 !== '' && !!+num1 && num2 !== '' && !!+num2) {
-        num1 = +num1;
-        num2 = +num2;
+  console.log(str1, str2, operator);
+  console.log(str1, !!str1, !!+str1);
 
-        if (operator === '+') result += num1 + num2;
-        else if (operator === '-') result += num1 - num2;
-        else if (operator === '*') result += num1 * num2;
-        else if (operator === '/') result += num1 / num2;
-        else if (operator === '**') result += num1 ** num2;
-        else result = '!Ошибка\nОператор не распознан';
+  if (str1 !== "" && !!str1 && str2 !== "" && !!str2) {
+    let num1 = +str1;
+    let num2 = +str2;
 
-    } else result = '!Ошибка\nЧисла не распознаны';
+    if (num1 >= 0 && num2 >= 0) {
 
-    return result;
+      if (operator === "+") result += num1 + num2;
+      else if (operator === "-") result += num1 - num2;
+      else if (operator === "*") result += num1 * num2;
+      else if (operator === "/") result += num1 / num2;
+      else if (operator === "**") result += num1 ** num2;
+			else result = "!Ошибка\nОператор не распознан";
+			
+		} else result = "!Ошибка\nЧисла не распознаны";
+		
+  } else result = "!Ошибка\nЧисла не распознаны";
+
+  return result;
 }
